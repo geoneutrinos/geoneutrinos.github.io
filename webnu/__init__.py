a55f60c83f0e5154b4596fc8e5df2718365ae60d
Andrew Barna <abarna@gmail.com>
2012-02-17 15:32:44 -1000

    Add basic js and css frameworks
    Also added a default error image for the maps. While not a lot of
    changes have been happning in the code, most of my time has been spent
    on reading of the documentations relating to jquery (which is the
    javascript framework that I will use to make things nice and
    interactive.)

--- webnu/__init__.py
+++ webnu/__init__.py
@@ -11,6 +11,8 @@ def main(global_config, **settings):
     config.add_route('plot', '/plot.png')
     config.add_view('webnu.views.render_plot',
                     route_name='plot')
+    config.add_static_view(name='css', path='webnu:static/css')
+    config.add_static_view(name='js', path='webnu:static/js')
     config.add_route('default', '/')
     config.add_view(route_name='default', renderer='templates/layout.jinja2')
     return config.make_wsgi_app()
