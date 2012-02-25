122069a2d89bf11f9158848a57a8d30126d6fe89
Andrew Barna <abarna@gmail.com>
2012-02-24 15:32:42 -1000

    Isolate basemap with multitasking
    I've done it! It took a bunch of time reading the various documentations
    about best practices with how to isolate the memory for each of these
    basemap instances (pyplot was the real unsafe thing). I finally found
    that multitasking was the way to go as I'm not IO bound (I don't need to
    block). This also means I should get a real good speed boost on systems
    with more than one processor. I've also adopted the practice of keeping
    any and all methods that will not be called by the pyramid routes out of
    the views.py file.

---
+++ webnu/geonu/plotting.py
@@ -0,0 +1,56 @@
+import matplotlib
+matplotlib.use('Agg')
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from crust_model import CrustModel
+
+from multiprocessing import Process
+import logging
+
+# Localization to make unicode sorting easy
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+
+def filename(request):
+    filename = u''
+
+    for key in sorted(request.GET.keys(), cmp=locale.strcoll):
+        value = ''.join(sorted(list(set(request.GET[key].lower())),
+            cmp=locale.strcoll))
+        filename = filename + u'_' + key.lower() + u'.' + value
+
+    filename = filename + u'.png'
+    return filename
+
+def plot(request, image_path):
+    logging.info('Map: Generating New Image')
+
+    # Do the fast thing before (crust model) before doing the slow thing
+    # that is the basemap init
+    crust = CrustModel()
+    crust.config(**request.GET)
+    
+    m = Basemap(projection = 'cyl',
+                llcrnrlat = -89, llcrnrlon = -180,
+                urcrnrlat = 89, urcrnrlon = 180,
+                resolution = 'l')
+    
+    lons, lats, data = crust.griddata()
+    nx = len(lons)
+    ny = len(lats)
+    map_data = m.transform_scalar(data, lons, lats, nx, ny)
+    im_data = m.imshow(map_data, interpolation = 'nearest')
+    m.drawcoastlines(linewidth = 0.2)
+    plt.axis('off')
+
+    plt.subplots_adjust(left = 0 , right = 1, top = 1, bottom = 0, wspace = 0,
+            hspace = 0)
+    logging.info(image_path)
+    plt.savefig(image_path,
+            bbox_inches = 'tight', format='png', 
+            dpi = 115, transparent = True, pad_inches = 0.01)
+
+def m_plot(request, image_path):
+    p = Process(target=plot, args=(request, image_path,))
+    p.start()
+    p.join()
