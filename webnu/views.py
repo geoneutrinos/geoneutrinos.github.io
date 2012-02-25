73ba23f01d708f2ca79f2b937f312015185e55b6
Andrew Barna <abarna@gmail.com>
2012-02-24 15:32:42 -1000

    Isolate basemap with multitasking
    I've done it! It took a bunch of time reading the various documentations
    about best practices with how to isolate the memory for each of these
    basemap instances (pyplot was the real unsafe thing). I finally found
    that multitasking was the way to go as I'm not IO bound (I don't need to
    block). This also means I should get a real good speed boost on systems
    with more than one processor. I've also adopted the practice of keeping
    any and all methods that will not be called by the pyramid routes out of
    the views.py file.

--- webnu/views.py
+++ webnu/views.py
@@ -1,32 +1,17 @@
 from pyramid.response import Response
 
-import matplotlib
-matplotlib.use('Agg')
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-from geonu.crust_model import CrustModel
-
 import logging
 import os
-
-# Localization to make unicode sorting easy
-import locale
-locale.setlocale(locale.LC_ALL, 'en_US.utf8')
-
+import geonu.plotting as gplt
 
 def my_view(request):
     return {'project':'webnu'}
 
 def render_plot(request):
     response = Response(content_type='image/png')
-    filename = u''
 
-    for key in sorted(request.GET.keys(), cmp=locale.strcoll):
-        value = ''.join(sorted(list(set(request.GET[key].lower())),
-            cmp=locale.strcoll))
-        filename = filename + u'_' + key.lower() + u'.' + value
+    filename = gplt.filename(request)
 
-    filename = filename + u'.png'
     logging.info('Filename:' + filename)
     here = os.path.dirname(__file__)
     image_path = os.path.join(here,'static','images', 'maps', filename)
@@ -37,31 +22,7 @@ def render_plot(request):
         logging.info('Map: Using cached image')
         return response
     except IOError: #assuming the map hasn't been generated yet
-        logging.info('Map: Generating New Image')
 
-        # Do the fast thing before (crust model) before doing the slow thing
-        # that is the basemap init
-        crust = CrustModel()
-        crust.config(**request.GET)
-        
-        m = Basemap(projection = 'cyl',
-                    llcrnrlat = -89, llcrnrlon = -180,
-                    urcrnrlat = 89, urcrnrlon = 180,
-                    resolution = 'l')
-        
-        lons, lats, data = crust.griddata()
-        nx = len(lons)
-        ny = len(lats)
-        map_data = m.transform_scalar(data, lons, lats, nx, ny)
-        im_data = m.imshow(map_data, interpolation = 'nearest')
-        m.drawcoastlines(linewidth = 0.2)
-        plt.axis('off')
-
-        plt.subplots_adjust(left = 0 , right = 1, top = 1, bottom = 0, wspace = 0,
-                hspace = 0)
-        plt.savefig(image_path,
-                bbox_inches = 'tight', format='png', 
-                dpi = 115, transparent = True, pad_inches = 0.01)
-
+        gplt.m_plot(request, image_path)
         response.app_iter = open(image_path, 'rb')
         return response
