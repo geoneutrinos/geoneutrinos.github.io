af4da7d3ed60002e67b12ec9a107465cf5462645
Andrew Barna <abarna@gmail.com>
2012-02-28 09:59:43 -1000

    fix interpolation and add ocean flag view
    Also add some documentation for the sources of the data

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid', 'pyramid_debugtoolbar']
+requires = ['pyramid==1.2', 'pyramid_debugtoolbar']
 
 setup(name='webnu',
       version='0.0',
