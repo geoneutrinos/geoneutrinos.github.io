cf17463b3ded670fa2c5f9752ecbb16c6bb5e497
Andrew Barna <abarna@gmail.com>
2013-09-01 20:16:54 -0700

    Add colorbar

--- webnu/static/js/nu_model.js
+++ webnu/static/js/nu_model.js
@@ -8,8 +8,8 @@ function updateThings(){
   plotsrc = "/plot.json?layers=shuml&output=t";
 
   container_width = $(".plot_container").width()
-  var width = container_width,
-      height = container_width/2;
+    var width = container_width,
+        height = container_width/2;
 
   var projection = d3.geo.equirectangular()
     .scale(153)
@@ -23,6 +23,11 @@ function updateThings(){
   d3.json(plotsrc, function(heatmap) {
     var dx = heatmap[0].length,
     dy = heatmap.length;
+
+  var min = d3.min(d3.max(heatmap));
+  var max = d3.max(d3.min(heatmap));
+
+  var step = (max - min)/5;
 
   // Fix the aspect ratio.
   // var ka = dy / dx, kb = height / width;
@@ -38,8 +43,8 @@ function updateThings(){
     .range([height, 0]);
 
   var color = d3.scale.linear()
-    .domain([95, 115, 135, 155, 175, 195])
-    .range(["#0a0", "#6c0", "#ee0", "#eb4", "#eb9", "#fff"]);
+    .domain([min, min + (step), min + (step * 2), min + (step *3), min + (step * 4), max])
+    .range(["#00008F", "#00f", "#0ff", "#ff0", "#f00", "#8f0000"]);
 
   d3.select(".plot_container").append("canvas")
     .attr("width", dx)
@@ -51,16 +56,16 @@ function updateThings(){
   var svg = d3.select(".plot_container").append("svg")
     .attr("viewBox", "0 0 960 480")
     .attr("preserveAspectRatio", "xMinYMin")
-    .attr("width", "100%")
+    .attr("width", width)
 
 
-  d3.json("/js/plates.json", function(collection) {
-    feature = svg.selectAll()
-    .data(collection.features)
-    .enter().append("svg:path")
-    .attr("d", path)
-    .attr("class", "plates")
-  });
+    d3.json("/js/plates.json", function(collection) {
+      feature = svg.selectAll()
+      .data(collection.features)
+      .enter().append("svg:path")
+      .attr("d", path)
+      .attr("class", "plates")
+    });
 
   // Compute the pixel colors; scaled by CSS.
   function drawImage(canvas) {
@@ -85,17 +90,142 @@ function updateThings(){
   }
   });
 
+
+// colorbar
+  var points = [
+    [0, 0],
+    [960, 0]
+      ];
+
+  function colormap(d){
+    var color1 = d3.interpolateLab("#00008f", "#00f");
+    var color2 = d3.interpolateLab("#00f", "#0ff");
+    var color3 = d3.interpolateLab("#0ff", "#ff0");
+    var color4 = d3.interpolateLab("#ff0", "#f00");
+    var color5 = d3.interpolateLab("#f00", "#8f0000");
+
+    if (d < 0.2) {
+      return color1(d * 5);
+    } else if (d >= 0.2 && d < 0.4){ 
+      return color2((d -0.2) * 5);
+    } else if (d >= 0.4 && d < 0.6){ 
+      return color3((d -0.4) * 5);
+    } else if (d >= 0.6 && d < 0.8){ 
+      return color4((d -0.6) * 5);
+    } else {
+      return color5((d -0.8) * 5);
+    }
+
+  }
+
+
+  var svg = d3.select(".colorbar").append("svg")
+    .attr("height", 40)
+    .attr("width", width)
+    .attr("viewBox", "0 0 960 40")
+    .attr("preserveAspectRatio", "xMinYMin");
+
+  var line = d3.svg.line()
+    .interpolate("basis");
+
+  svg.selectAll("path")
+    .data(quad(sample(line(points), 8)))
+    .enter().append("path")
+    .style("fill", function(d) { return colormap(d.t); })
+    .style("stroke", function(d) { return colormap(d.t); })
+    .attr("d", function(d) { return lineJoin(d[0], d[1], d[2], d[3], 50); });
+
+  // Sample the SVG path string "d" uniformly with the specified precision.
+  function sample(d, precision) {
+    var path = document.createElementNS(d3.ns.prefix.svg, "path");
+    path.setAttribute("d", d);
+
+    var n = path.getTotalLength(), t = [0], i = 0, dt = precision;
+    while ((i += dt) < n) t.push(i);
+    t.push(n);
+
+    return t.map(function(t) {
+      var p = path.getPointAtLength(t), a = [p.x, p.y];
+      a.t = t / n;
+      return a;
+    });
+  }
+
+  // Compute quads of adjacent points [p0, p1, p2, p3].
+  function quad(points) {
+    return d3.range(points.length - 1).map(function(i) {
+      var a = [points[i - 1], points[i], points[i + 1], points[i + 2]];
+      a.t = (points[i].t + points[i + 1].t) / 2;
+      return a;
+    });
+  }
+
+  // Compute stroke outline for segment p12.
+  function lineJoin(p0, p1, p2, p3, width) {
+    var u12 = perp(p1, p2),
+        r = width / 2,
+        a = [p1[0] + u12[0] * r, p1[1] + u12[1] * r],
+        b = [p2[0] + u12[0] * r, p2[1] + u12[1] * r],
+        c = [p2[0] - u12[0] * r, p2[1] - u12[1] * r],
+        d = [p1[0] - u12[0] * r, p1[1] - u12[1] * r];
+
+    if (p0) { // clip ad and dc using average of u01 and u12
+      var u01 = perp(p0, p1), e = [p1[0] + u01[0] + u12[0], p1[1] + u01[1] + u12[1]];
+      a = lineIntersect(p1, e, a, b);
+      d = lineIntersect(p1, e, d, c);
+    }
+
+    if (p3) { // clip ab and dc using average of u12 and u23
+      var u23 = perp(p2, p3), e = [p2[0] + u23[0] + u12[0], p2[1] + u23[1] + u12[1]];
+      b = lineIntersect(p2, e, a, b);
+      c = lineIntersect(p2, e, d, c);
+    }
+
+    return "M" + a + "L" + b + " " + c + " " + d + "Z";
+  }
+
+  // Compute intersection of two infinite lines ab and cd.
+  function lineIntersect(a, b, c, d) {
+    var x1 = c[0], x3 = a[0], x21 = d[0] - x1, x43 = b[0] - x3,
+        y1 = c[1], y3 = a[1], y21 = d[1] - y1, y43 = b[1] - y3,
+        ua = (x43 * (y1 - y3) - y43 * (x1 - x3)) / (y43 * x21 - x43 * y21);
+    return [x1 + ua * x21, y1 + ua * y21];
+  }
+
+  // Compute unit vector perpendicular to p01.
+  function perp(p0, p1) {
+    var u01x = p0[1] - p1[1], u01y = p1[0] - p0[0],
+        u01d = Math.sqrt(u01x * u01x + u01y * u01y);
+    return [u01x / u01d, u01y / u01d];
+  }
+
 }
 $(document).ready(function() {
-
+  var width = $(".plot_container").width();
+  $(".plot_container").height(width/2);
+  $(".colorbar").height(40);
   updateThings();
 
-  // do stuff
-  $('input').click(updateThings);
-  updateThings;
+
+  //UI Components
+  // Basic Controlls
+  
+  // Layers
+  // Plate Boundries
+  $("#plate_boundries_on").click(function() {
+    $(".plates").css("visibility", "visible");
+  });
+  $("#plate_boundries_off").click(function() {
+    $(".plates").css("visibility", "hidden");
+  });
 });
+
+//Keep the canvas the same size as the svc (which automatically scales)
 $(window).resize(function() {
+  var width = $(".plot_container").width();
+  $(".plot_container").height(width/2);
   var width = $(".plot_container").width();
   $("canvas").width(width);
   $("canvas").height(width/2);
+$("svg").width(width);
 });
