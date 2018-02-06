3b981ca3b9475086e268d8a704aa40bff50910d5
Andrew Barna <abarna@gmail.com>
2018-02-05 20:12:11 -0800

    change some defaults and hide user reactor info when not inuse

--- _jsx/reactors.jsx
+++ _jsx/reactors.jsx
@@ -53,7 +53,7 @@ var detectorPosition = {
   "lon": -0.82
 };
 
-var followMouse = true;
+var followMouse = false;
 
 var loadFactor = {
   ystart: "2016",
@@ -294,7 +294,7 @@ const detectorCircles = detectorLocations.map(function(data){
 });
 
 
-var map = L.map('map_container').setView([0, 0], 1);
+var map = L.map('map_container', {worldCopyJump:true}).setView([0, 0], 1);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -806,15 +806,22 @@ var StatsPanel = React.createClass({
     }
   },
   render: function(){
+    function userDisplay(use){
+      if (use){
+        return "inline";
+      } else {
+        return "none";
+      }
+    };
     return (
         <div>
           <i>R</i><sub>total</sub> = {this.state.total_tnu.toFixed(1)} TNU<br />
           <i>R</i><sub>reac</sub> = {this.state.reactors_tnu.toFixed(1)} TNU<br />
           <i>R</i><sub>closest</sub> = {this.state.closest_tnu.toFixed(1)} TNU ({(this.state.closest_tnu/this.state.total_tnu * 100).toFixed(1)} % of total)<br />
           <i>D</i><sub>closest</sub> = {this.state.closest_distance.toFixed(2)} km<br />
-          <i>D</i><sub>user</sub> = {this.state.custom_distance.toFixed(3)} km<br />
+      <span style={{"display": userDisplay(customReactor.use)}}><i>D</i><sub>user</sub> = {this.state.custom_distance.toFixed(3)} km<br /></span>
           <i>R</i><sub>E &lt; 3.275 MeV</sub> = {this.state.total_tnu_geo.toFixed(1)} TNU<br />
-      <i>R</i><sub>geo</sub> = {this.state.geo_tnu.toFixed(1)} TNU (U ={this.state.geo_u_tnu.toFixed(1)}, Th = {this.state.geo_th_tnu.toFixed(1)})<br />
+      <i>R</i><sub>geo</sub> = {this.state.geo_tnu.toFixed(1)} TNU (U = {this.state.geo_u_tnu.toFixed(1)}, Th = {this.state.geo_th_tnu.toFixed(1)})<br />
           <i>Th/U</i><sub>geo</sub> = {this.state.geo_r.toFixed(1)}<br />
       <small>1 TNU = 1 event/10<sup>32</sup> free protons/year</small><br />
           <small>1 kT H<sub>2</sub>O contains 0.668559x10<sup>32</sup> free protons</small>
@@ -1146,7 +1153,7 @@ var CalculatorPanel = React.createClass({
 
       <FormGroup controlId="signal">
         <Col componentClass={ControlLabel} sm={4}>
-          Signal
+          Signal (bkgnd)
         </Col>
         <Col sm={8}>
           <FormControl onChange={this.handleUserInput} value={this.state.signal} componentClass="select">
@@ -1219,7 +1226,7 @@ var CalculatorPanel = React.createClass({
       </FormGroup>
 
       </Form>
-      <div>Sigma = Signal * sqrt(Time) / sqrt(Signal + 2 * Background)</div>
+      <div> N<sub>σ</sub> = Signal * sqrt(Time) / sqrt(Signal + 2 * Background)</div>
     </Panel>
     )
   }
@@ -1308,7 +1315,7 @@ var ReactorListPanel = React.createClass({
       )
     })
     return (
-      <Panel  header="Reactor List (Name, Power[MW], Type, Override)">
+      <Panel  header="Reactor List: Name, Power(MW), Type, Power Override">
         <div style={{width: "100%", maxHeight:"40vh", overflowX:"scroll"}}>
         <Table fill condensed striped>
           <thead>
